baf4532c79914c24c3d7a8a38066be8b85f890c5
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2015-07-18 13:24:53 -0700

    sort, transfer 2015 06 14 from where to git

---
+++ go.mm
@@ -0,0 +1,112 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="git, go" LOCALIZED_STYLE_REF="default" ID="ID_1201843186" CREATED="1336484964936" MODIFIED="1437249755377"><hook NAME="MapStyle" zoom="1.128">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="800" COLOR="#000000" STYLE="as_parent">
+<font NAME="Courier" SIZE="16" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" MAX_WIDTH="800" COLOR="#cc00ff">
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" MAX_WIDTH="800">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#009999" STYLE="fork" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ff7700" BACKGROUND_COLOR="#c6f7f9" STYLE="fork" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#008600" BACKGROUND_COLOR="#f5e1db" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" MAX_WIDTH="800">
+<icon BUILTIN="yes"/>
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000099" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#006600" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#996600" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#660066" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="clarifications are in blue" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" POSITION="right" ID="ID_69958477" CREATED="1437250142235" MODIFIED="1437250147042">
+<node TEXT="to clarify things I wrote short in the parent" ID="ID_1848181419" CREATED="1437250147899" MODIFIED="1437250162353"/>
+</node>
+<node TEXT="2015 6 14" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" POSITION="right" ID="ID_1175391778" CREATED="1437250735163" MODIFIED="1437250748053"/>
+<node TEXT="sunlight: sufficient punishment" POSITION="right" ID="ID_1600718413" CREATED="1433797393550" MODIFIED="1437250591702">
+<node TEXT="don&apos;t try to punish [citizens]. tell the truth, ask about it, thereby perhaps induce others to recognize it. perhaps the state is right to isolate the dangerous, but not to make them feel bad about it." ID="ID_1847686788" CREATED="1433797190255" MODIFIED="1437250482063"/>
+<node TEXT="we want to see reality, it is inherent to our minds, and grappling with the fact that you did something bad is hard, perhaps already sufficient punishment." ID="ID_1907501110" CREATED="1437250524251" MODIFIED="1437250537483"/>
+<node TEXT="perhaps don&apos;t belabor that it was bad, but do make sure someone recognizes all the elements of what they did" ID="ID_1261473377" CREATED="1437250538210" MODIFIED="1437250559970"/>
+</node>
+<node TEXT="fun =&gt; love + responsb" POSITION="right" ID="ID_465755849" CREATED="1437250047835" MODIFIED="1437250186958">
+<node TEXT="love because fun, leads to responsibility" ID="ID_1978101503" CREATED="1433829881402" MODIFIED="1437250090953"/>
+<node TEXT="e.g. unless love the child, parenting no fun" ID="ID_976947062" CREATED="1437250091219" MODIFIED="1437250102258"/>
+</node>
+<node TEXT="&amp; opporm proc effic" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_1372673622" CREATED="1437249697998" MODIFIED="1437249858552">
+<node TEXT="opportunism" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1845367409" CREATED="1437249834603" MODIFIED="1437249841547"/>
+<node TEXT="don&apos;t waste a process" ID="ID_1583610447" CREATED="1434312279722" MODIFIED="1434312283623"/>
+</node>
+<node TEXT="info bias" POSITION="right" ID="ID_103659948" CREATED="1437249885138" MODIFIED="1437250357103">
+<node TEXT="!= cog bias" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1972347690" CREATED="1437250361011" MODIFIED="1437250406735">
+<node LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_652084445" CREATED="1437250407216" MODIFIED="1437250417304"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      distinct from
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="if nobody told you about pain except because you could help" ID="ID_1949113058" CREATED="1434312464594" MODIFIED="1434312474000"/>
+</node>
+<node TEXT="info diet" POSITION="right" ID="ID_1271431608" CREATED="1437250697579" MODIFIED="1437250700890">
+<node TEXT="[topic. information?] density of npr high" ID="ID_1547223750" CREATED="1433790328583" MODIFIED="1437250660261"/>
+</node>
+<node TEXT="distant act" POSITION="right" ID="ID_177517417" CREATED="1437249925355" MODIFIED="1437250715471">
+<node TEXT="action at a distance" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_716234217" CREATED="1437249949203" MODIFIED="1437249952643"/>
+<node TEXT="help by understg" ID="ID_950683569" CREATED="1434312478699" MODIFIED="1434312489639"/>
+</node>
+<node TEXT="text graph rels" POSITION="right" ID="ID_1615294614" CREATED="1437250003027" MODIFIED="1437250011666">
+<node TEXT="rel &quot;in _ there is _&quot; &gt; includes" ID="ID_646208813" CREATED="1434317621077" MODIFIED="1434317633076"/>
+</node>
+<node TEXT="art proc" POSITION="right" ID="ID_600584789" CREATED="1437250285915" MODIFIED="1437250287770">
+<node TEXT="self-aware lyrical flow coprocesses" ID="ID_1536669145" CREATED="1433797468119" MODIFIED="1433797490345">
+<node TEXT="in metaphor recounting the others&apos; adventures" ID="ID_44051061" CREATED="1433797501511" MODIFIED="1437250229686"/>
+</node>
+</node>
+</node>
+</map>
