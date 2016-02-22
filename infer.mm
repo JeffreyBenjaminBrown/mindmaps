78b9aeeddaa0c96d00a021a4fae0e13c7a1ee275
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2016-02-21 18:48:21 -0800

    fresh.mm: sort, export 3 days

---
+++ infer.mm
@@ -0,0 +1,121 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="git / infer" LOCALIZED_STYLE_REF="default" ID="ID_1201843186" CREATED="1336484964936" MODIFIED="1456108708956"><hook NAME="MapStyle" zoom="1.101">
+    <properties show_icon_for_attributes="true" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="800" COLOR="#000000" STYLE="as_parent">
+<font NAME="Courier" SIZE="16" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" MAX_WIDTH="800" COLOR="#cc00ff">
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" MAX_WIDTH="800">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#009999" STYLE="fork" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#ff7700" BACKGROUND_COLOR="#c6f7f9" STYLE="fork" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#008600" BACKGROUND_COLOR="#f5e1db" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" MAX_WIDTH="800">
+<icon BUILTIN="yes"/>
+<font NAME="Courier" SIZE="16"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000099" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#006600" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#996600" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#660066" MAX_WIDTH="800">
+<font NAME="Courier" SIZE="16" BOLD="true"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="2016 02 21" POSITION="right" ID="ID_341740265" CREATED="1456108522102" MODIFIED="1456108527771">
+<node TEXT="= info mgmt \ proc" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1015212872" CREATED="1456105941452" MODIFIED="1456105949893">
+<node TEXT="mgmt = motivd modif" ID="ID_427221802" CREATED="1456105959500" MODIFIED="1456105970417"/>
+</node>
+<node TEXT="~ emot" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_303548399" CREATED="1456105504500" MODIFIED="1456105508150"/>
+<node TEXT="proof / purposes" ID="ID_1956622229" CREATED="1456106318733" MODIFIED="1456106466117">
+<node TEXT="&quot;I am _; with _ we could be _.&quot;" ID="ID_910974636" CREATED="1456106363148" MODIFIED="1456106438685">
+<node TEXT="persuade to join or give" ID="ID_1727387761" CREATED="1456106445149" MODIFIED="1456106448170"/>
+</node>
+<node TEXT="&quot;Things are good.&quot;" ID="ID_1908851311" CREATED="1456106391101" MODIFIED="1456106404764">
+<node TEXT="you are safe, _ is not a danger, _ can afford _" ID="ID_1391569517" CREATED="1456106405306" MODIFIED="1456106417714"/>
+</node>
+</node>
+<node TEXT="not mine ~ not real" ID="ID_1656412830" CREATED="1451790544591" MODIFIED="1456104976028"/>
+<node TEXT="relatively safe in the truth" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" ID="ID_728959180" CREATED="1451797687548" MODIFIED="1456105154244"/>
+<node TEXT="before confrontation not the time to review your stereotypes" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_214167023" CREATED="1451798929221" MODIFIED="1451927597542">
+<font BOLD="false"/>
+<node TEXT="wikispastic" ID="ID_363732480" CREATED="1451798948036" MODIFIED="1451798950300"/>
+<node TEXT="\group" ID="ID_1279187342" CREATED="1456105346692" MODIFIED="1456105350345"/>
+</node>
+<node TEXT="invest belief" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1697043294" CREATED="1451799580733" MODIFIED="1451799585223"/>
+<node TEXT="&quot;fear is a bad advisor&quot;" ID="ID_131303092" CREATED="1451801815054" MODIFIED="1451801818621"/>
+<node TEXT="frame bad and not mine : irrelevant" ID="ID_384921069" CREATED="1456105561796" MODIFIED="1456105579708">
+<node TEXT="I can state to myself that, e.g., dignity is a bad frame, but if it is not one I use, then the statement is aggression." ID="ID_1555739610" CREATED="1456105601180" MODIFIED="1456105636530"/>
+</node>
+<node TEXT="devnull ~ prune" ID="ID_390902957" CREATED="1451805063977" MODIFIED="1451805067154">
+<node TEXT="in choosing a categ* for something, view in the categ how oft it is visited, know that some categs are effectively condemnation, devnull" ID="ID_929254496" CREATED="1451805069073" MODIFIED="1451805104591">
+<node TEXT="that is, in choosing the kind of categ from which one might travel to the thing, rather than the reverse" ID="ID_26719963" CREATED="1451805109878" MODIFIED="1451805122618"/>
+</node>
+</node>
+<node TEXT="like malicious quoting (fox news) it is for me to mention, after a topic arises, the worst about it" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1321527881" CREATED="1451895995295" MODIFIED="1451926874151">
+<font BOLD="false"/>
+</node>
+<node TEXT="rejection : incomplete" ID="ID_1941180269" CREATED="1456108274270" MODIFIED="1456108277627"/>
+<node TEXT="hate : incomplete" ID="ID_828403853" CREATED="1451926883215" MODIFIED="1456108282347">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1941180269" STARTINCLINATION="70;0;" ENDINCLINATION="70;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Hate is mostly rejection, and rejection is an incomplete choice." ID="ID_1363492670" CREATED="1451880218983" MODIFIED="1451880219941">
+<node TEXT="td" ID="ID_1365938806" CREATED="1451880269542" MODIFIED="1451880270687"/>
+</node>
+</node>
+<node TEXT="for every _ know a positive interpretation" ID="ID_626636345" CREATED="1451888470739" MODIFIED="1451888485169">
+<node TEXT="e.g. (mus) an alternative to (seq: 0 min 10 min)" ID="ID_944453686" CREATED="1451888493505" MODIFIED="1456107352373">
+<node TEXT="minor chords" ID="ID_960431138" CREATED="1451888519502" MODIFIED="1456107331957"/>
+<node TEXT="just add 8 in the bass" ID="ID_1817958292" CREATED="1456107332141" MODIFIED="1456107341453"/>
+</node>
+</node>
+<node TEXT="before calling [who] [bad], consider diversity among them" ID="ID_1017991787" CREATED="1451888189095" MODIFIED="1451888218191">
+<node TEXT="\ prejudice(against)" ID="ID_269893" CREATED="1451888220608" MODIFIED="1456107654354"/>
+</node>
+<node TEXT="conf bias" ID="ID_1624196403" CREATED="1450414460003" MODIFIED="1450414465239">
+<node TEXT="= try to prove but not disprove" ID="ID_175578039" CREATED="1450414468045" MODIFIED="1456108137781"/>
+<node TEXT="reverse also interesting" ID="ID_1026299050" CREATED="1456108115558" MODIFIED="1456108128755">
+<node TEXT="internet antagonism" ID="ID_1552300004" CREATED="1456108129374" MODIFIED="1456108132995"/>
+</node>
+</node>
+<node TEXT="skinner pigeon dance" ID="ID_1752759378" CREATED="1450414105204" MODIFIED="1450414109650"/>
+<node TEXT="unplug a concept" ID="ID_1830918680" CREATED="1451861641052" MODIFIED="1451861676402">
+<node TEXT="unplug at least socially" ID="ID_1156436902" CREATED="1451861677068" MODIFIED="1451861682836"/>
+<node TEXT="see no evil, hear no evil, speak no evil" ID="ID_280155211" CREATED="1451861624300" MODIFIED="1451861629900"/>
+</node>
+</node>
+</node>
+</map>
